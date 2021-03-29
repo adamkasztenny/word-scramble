ffca729b3e6628adbf937685e20c0af9313e4945
Adam Kasztenny <adamkasztenny@gmail.com>
2021-03-28 20:51:10 -0400

    Improves App component test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,6 +4,28 @@ import App from "./App";
 
 test("renders title", () => {
   render(<App />);
+
   const title = screen.getByText(/Word Scramble/i);
   expect(title).toBeInTheDocument();
+});
+
+test("renders initial question number", () => {
+  render(<App />);
+
+  const firstQuestion = screen.getByText(/Question 1/i);
+  expect(firstQuestion).toBeInTheDocument();
+});
+
+test("renders initial score", () => {
+  render(<App />);
+
+  const initialScore = screen.getByText(/Score: 0/i);
+  expect(initialScore).toBeInTheDocument();
+});
+
+test("renders initial time remaining", () => {
+  render(<App />);
+
+  const initialTimeRemaining = screen.getByText(/Time Remaining: 3:00/i);
+  expect(initialTimeRemaining).toBeInTheDocument();
 });
